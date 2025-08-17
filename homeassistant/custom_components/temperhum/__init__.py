4dbb50628c5e2af25b639427114733609b4439e0
bitkittyy <bitkittyy@localhost.localdomain>
2025-08-17 10:32:28 -0700

    Add debug logging to temperhum custom component __init__.py

--- homeassistant/custom_components/temperhum/__init__.py
+++ homeassistant/custom_components/temperhum/__init__.py
@@ -1,8 +1,13 @@
 """The TEMPerHUM custom component for Home Assistant."""
+import logging
+
+_LOGGER = logging.getLogger(__name__)
+
 DOMAIN = "temperhum"
 
 def setup(hass, config):
     """Set up the TEMPerHUM component."""
+    _LOGGER.debug("Setting up TEMPerHUM custom component")
     # This component does not have any global configuration.
     # All setup will be done in the sensor platform.
     return True
